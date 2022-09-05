948eac5c72b95d4f9db2005b5a0588873632bbd2
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2022-09-05 22:36:43 +0530

    footer added

---
+++ footer/footer.js
@@ -0,0 +1,8 @@
+
+function changebgColor() {
+    const content=document.querySelector(".innerinputbox");
+    content.style.backgroundColor="white";
+
+    const content2=document.querySelector("#emailinput");
+    content2.style.backgroundColor="white";
+}
