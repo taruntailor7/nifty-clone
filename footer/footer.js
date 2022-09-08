11c14f51010549f12f5edcd61dae615c8b0db4ee
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2022-09-09 05:00:10 +0530

    everything done

--- footer/footer.js
+++
@@ -1,8 +0,0 @@
-
-function changebgColor() {
-    const content=document.querySelector(".innerinputbox");
-    content.style.backgroundColor="white";
-
-    const content2=document.querySelector("#emailinput");
-    content2.style.backgroundColor="white";
-}
