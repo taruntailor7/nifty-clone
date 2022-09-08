295f02d9832bcc795e0d4fa68e8a3cce1fe77abe
rajbaghelrahul <rajbaghelrahul@gmail.com>
2022-09-09 00:31:19 +0530

    homePage

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 let div1 = document.createElement("div");
 let heading1 = document.createElement("h1");
 heading1.innerText = "Establish Knowledge Hub";
-// let para = document.createElement("h4");
-// para.innerText = "Empower all members to share ideas, finalize specifications, & gather feedback in real-time with chat and one-click video calls. span Learn more >";
+let para = document.createElement("p");
+para.innerText = "Empower all members to share ideas, finalize specifications, & gather feedback in real-time with chat and one-click video calls.";
 
-div1.append(heading1);
+div1.append(heading1, para);
 
 let divMain = document.getElementById("visible");
 let image = document.createElement("img");
@@ -14,13 +14,10 @@ image.src = "https://niftypm.com/_nuxt/img/large@1x.f90d042.webp";
 divMain.append(div1, image);
 
 document.getElementById("divA").addEventListener("click",function(){
-    // let divMain = document.getElementById("visible");
-    // let image = document.createElement("img");
-    // let div1 = document.createElement("div");
-    // let heading1 = document.createElement("h1");
     divMain.innerHTML = "";
     heading1.innerText = "Establish Knowledge Hub";
-    div1.append(heading1);    
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
+    div1.append(heading1, para);
     image.src = "https://niftypm.com/_nuxt/img/large@1x.f90d042.webp";
     divMain.append(div1,image);
 })
@@ -30,7 +27,8 @@ document.getElementById("divB").addEventListener("click",function(){
     // let image = document.createElement("img");
     divMain.innerHTML = "";
     heading1.innerText = "Collaborate on Tasks";
-    div1.append(heading1); 
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
+    div1.append(heading1, para);
     image.src = "https://niftypm.com/_nuxt/img/large@1x.dd989ad.webp";
     divMain.append(div1, image);
 })
@@ -40,7 +38,8 @@ document.getElementById("divC").addEventListener("click",function(){
     // let image = document.createElement("img");
     divMain.innerHTML = "";
     heading1.innerText = "Automate Reporting";
-    div1.append(heading1); 
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
+    div1.append(heading1, para); 
     image.src = "https://niftypm.com/_nuxt/img/large@1x.4472281.png";
     divMain.append(div1, image);
 })
@@ -49,8 +48,10 @@ document.getElementById("divD").addEventListener("click",function(){
     // let divMain = document.getElementById("visible");
     // let image = document.createElement("img");
     divMain.innerHTML = "";
+    heading1.innerText = "Establish Knowledge Hub";
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
     heading1.innerText = "Create Docs & Wikis";
-    div1.append(heading1); 
+    div1.append(heading1, para); 
     image.src = "https://niftypm.com/_nuxt/img/large@1x.e2a66e9.webp";
     divMain.append(div1, image);
 })
@@ -64,8 +65,8 @@ document.getElementById("divD").addEventListener("click",function(){
 let div2 = document.createElement("div");
 let heading2 = document.createElement("h1");
 heading2.innerText = "Establish Knowledge Hub";
-// let para = document.createElement("h4");
-// para.innerText = "Empower all members to share ideas, finalize specifications, & gather feedback in real-time with chat and one-click video calls. span Learn more >";
+// let para = document.createElement("p");
+para.innerText = "Empower all members to share ideas, finalize specifications, & gather feedback in real-time with chat and one-click video calls. span Learn more >";
 
 div2.append(heading2);
 
@@ -75,42 +76,36 @@ image2.src = "https://niftypm.com/_nuxt/img/large@1x.f90d042.webp";
 divMain2.append(div2, image2);
 
 document.getElementById("divE").addEventListener("click",function(){
-    // let divMain = document.getElementById("visible");
-    // let image = document.createElement("img");
-    // let div1 = document.createElement("div");
-    // let heading1 = document.createElement("h1");
     divMain2.innerHTML = "";
     heading2.innerText = "Establish Knowledge Hub";
-    div2.append(heading2);
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
+    div2.append(heading2, para);
     image2.src = "https://niftypm.com/_nuxt/img/large@1x.f90d042.webp";
     divMain2.append(div2,image2);
 })
 
 document.getElementById("divF").addEventListener("click",function(){
-    // let divMain = document.getElementById("visible");
-    // let image = document.createElement("img");
     divMain2.innerHTML = "";
     heading2.innerText = "Collaborate on Tasks";
-    div2.append(heading2); 
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
+    div2.append(heading2, para); 
     image2.src = "https://niftypm.com/_nuxt/img/large@1x.dd989ad.webp";
     divMain2.append(div2, image2);
 })
 
 document.getElementById("divG").addEventListener("click",function(){
-    // let divMain = document.getElementById("visible");
-    // let image = document.createElement("img");
     divMain2.innerHTML = "";
     heading2.innerText = "Automate Reporting";
-    div2.append(heading2); 
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
+    div2.append(heading2, para);
     image2.src = "https://niftypm.com/_nuxt/img/large@1x.4472281.png";
     divMain2.append(div2, image2);
 })
 
 document.getElementById("divH").addEventListener("click",function(){
-    // let divMain = document.getElementById("visible");
-    // let image = document.createElement("img");
     divMain2.innerHTML = "";
     heading2.innerText = "Create Docs & Wikis";
+    para.innerText = "Flexibly organize, prioritize, and track daily work with a high level of detail using Kanban, List, Timeline, Calendar, and Swimlane views.";
     div2.append(heading2); 
     image2.src = "https://niftypm.com/_nuxt/img/large@1x.e2a66e9.webp";
     divMain2.append(div2, image2);
